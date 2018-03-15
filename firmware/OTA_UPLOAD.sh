412d8841c035ffabe520b902837d9ca935e7658b
Thorsten Ludewig <th@l9g.de>
2018-03-15 07:56:28 +0100

    firmware binary 1.1.4 and OTA_UPLOAD script added

---
+++ firmware/OTA_UPLOAD.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+$HOME/.platformio/penv/bin/python \
+  $HOME/.platformio/packages/tool-espotapy/espota.py \
+  --debug --progress -i "wifi-socket-1.local" \
+  --auth="otapass" -f "firmware-1.1.4.bin"
