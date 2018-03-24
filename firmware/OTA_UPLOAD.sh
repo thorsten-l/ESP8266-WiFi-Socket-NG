76bd3b7e24aba352742d91aadd0a2bf7211e6729
Thorsten Ludewig <th@l9g.de>
2018-03-24 22:03:57 +0100

    Release 1.1.5

--- firmware/OTA_UPLOAD.sh
+++ firmware/OTA_UPLOAD.sh
@@ -2,4 +2,4 @@
 $HOME/.platformio/penv/bin/python \
   $HOME/.platformio/packages/tool-espotapy/espota.py \
   --debug --progress -i "wifi-socket-1.local" \
-  --auth="otapass" -f "firmware-1.1.4.bin"
+  --auth="otapass" -f "firmware-1.1.5.bin"
--- firmware/USB_UPLOAD.sh
+++ firmware/USB_UPLOAD.sh
@@ -1,2 +1,2 @@
  #!/bin/sh
- $HOME/.platformio/packages/tool-esptool/esptool -cd ck -cb 115200 -cp /dev/cu.usbserial-A702LC9S -cf firmware-1.1.4.bin
+ $HOME/.platformio/packages/tool-esptool/esptool -cd ck -cb 115200 -cp /dev/cu.usbserial-A702LC9S -cf firmware-1.1.5.bin
