59b4fa3128f09b4567fe8d73e3a4d75bef38835a
Thorsten Ludewig <th@l9g.de>
2018-07-22 15:51:34 +0200

    change firmware version 1.1.6

--- firmware/OTA_UPLOAD.sh
+++ firmware/OTA_UPLOAD.sh
@@ -2,4 +2,4 @@
 $HOME/.platformio/penv/bin/python \
   $HOME/.platformio/packages/tool-espotapy/espota.py \
   --debug --progress -i "wifi-socket-1.local" \
-  --auth="otapass" -f "firmware-1.1.5.bin"
+  --auth="otapass" -f "firmware-1.1.6.bin"
--- firmware/USB_UPLOAD.sh
+++ firmware/USB_UPLOAD.sh
@@ -1,2 +1,2 @@
  #!/bin/sh
- $HOME/.platformio/packages/tool-esptool/esptool -cd ck -cb 115200 -cp /dev/cu.usbserial-A702LC9S -cf firmware-1.1.5.bin
+ $HOME/.platformio/packages/tool-esptool/esptool -cd ck -cb 115200 -cp /dev/cu.SLAB_USBtoUART -cf firmware-1.1.6.bin
